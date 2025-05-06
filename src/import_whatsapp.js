1ec73e5f19b0ea06a61cd0af1b95c0279e6d854c
MarcosMoreu <marcos.moreu@protonmail.com>
2025-05-06 15:58:05 +0300

    parser to accept d/m/yy and catch any other format

--- src/import_whatsapp.js
+++ src/import_whatsapp.js
@@ -325,12 +325,15 @@ const setImgMsgRegex = (fileType) => {
 		// console.info("android format");
 		// Android format
 		messageRegex =
-			/(\d{2,4}\/\d{2}\/\d{2,4}),?\s(\d{1,2}:\d{2})(?:\s?(?:AM|PM|am|pm))?\s-\s(.*?):[\t\f\cK ]((.|\n)*?)(?=(\n\d{2,4}\/\d{2}\/\d{2,4})|$)/g;
-		// Regex to match and capture image filenames in messages
+		/(\d{1,4}\/\d{1,2}\/\d{1,4}),?\s(\d{1,2}:\d{2}(?::\d{2})?(?:\s?(?:AM|PM|am|pm))?)?\s-\s(.*?):[\t\f\cK ]((.|\n)*?)(?=(\n\d{1,4}\/\d{1,2}\/\d{1,4})|$)/g;
+			// Regex to match and capture image filenames in messages
 		imgFileRegex = /\b([\w\-_]*\.(jpg|jpeg|png|gif))\s\(file attached\)/gim;
 	} else {
-		console.error("Unknown file format");
-		return [null, null];
+		console.log("Unknown file type, defaulting to Android format");
+		messageRegex =
+		/(\d{1,4}\/\d{1,2}\/\d{1,4}),?\s(\d{1,2}:\d{2}(?::\d{2})?(?:\s?(?:AM|PM|am|pm))?)?\s-\s(.*?):[\t\f\cK ]((.|\n)*?)(?=(\n\d{1,4}\/\d{1,2}\/\d{1,4})|$)/g;
+			// Regex to match and capture image filenames in messages
+		imgFileRegex = /\b([\w\-_]*\.(jpg|jpeg|png|gif))\s\(file attached\)/gim;
 	}
 	return [messageRegex, imgFileRegex];
 };
